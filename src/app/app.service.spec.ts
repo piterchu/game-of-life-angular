e790c347dffc919f7e01da0264353962ee73288f
MALZIEUX Hugo <hugo.malzieux.contact@gmail.com>
2020-09-22 08:46:03 +0200

    Add findNeighbours() tests

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -4,13 +4,101 @@ import { AppService } from './app.service';
 
 describe('AppService', () => {
   let service: AppService;
-
+  let dataArray: Array<Array<number>>;
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(AppService);
+
+    dataArray = new Array<Array<number>>();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('findNeighbours() method', () => {
+    it('should return 1 because actual cell is alive and it have 2 neighbours', () => {
+      // Arrange
+      dataArray[0] = [1, 0, 0];
+      dataArray[1] = [1, 1, 0];
+      dataArray[2] = [0, 0, 0];
+      // Act
+      const result = AppService.findNeighbours(dataArray, 1, 1);
+
+      // Assert
+      expect(result).toEqual(1);
+    });
+
+    it('should return 1 because actual cell is alive and it have 3 neighbours', () => {
+      // Arrange
+      dataArray[0] = [0, 0, 1];
+      dataArray[1] = [1, 1, 0];
+      dataArray[2] = [0, 0, 1];
+      // Act
+      const result = AppService.findNeighbours(dataArray, 1, 1);
+
+      // Assert
+      expect(result).toEqual(1);
+    });
+
+    it('should return 0 because actual cell is alive and it have more than 3 neighbours', () => {
+      // Arrange
+      dataArray[0] = [0, 1, 1];
+      dataArray[1] = [1, 1, 0];
+      dataArray[2] = [0, 0, 1];
+      // Act
+      const result = AppService.findNeighbours(dataArray, 1, 1);
+
+      // Assert
+      expect(result).toEqual(0);
+    });
+
+    it('should return 0 because actual cell is alive and it have less than 2 neighbours', () => {
+      // Arrange
+      dataArray[0] = [1, 0, 0];
+      dataArray[1] = [0, 1, 0];
+      dataArray[2] = [0, 0, 0];
+      // Act
+      const result = AppService.findNeighbours(dataArray, 1, 1);
+
+      // Assert
+      expect(result).toEqual(0);
+    });
+
+    it('should return 0 because actual cell is not alive and it have less than 3 neighbours', () => {
+      // Arrange
+      dataArray[0] = [1, 0, 0];
+      dataArray[1] = [1, 0, 0];
+      dataArray[2] = [0, 0, 0];
+      // Act
+      const result = AppService.findNeighbours(dataArray, 1, 1);
+
+      // Assert
+      expect(result).toEqual(0);
+    });
+
+    it('should return 0 because actual cell is not alive and it have more than 3 neighbours', () => {
+      // Arrange
+      dataArray[0] = [0, 1, 1];
+      dataArray[1] = [1, 0, 0];
+      dataArray[2] = [0, 0, 1];
+      // Act
+      const result = AppService.findNeighbours(dataArray, 1, 1);
+
+      // Assert
+      expect(result).toEqual(0);
+    });
+
+    it('should return 1 because actual cell is alive and it have exactly 3 neighbours', () => {
+      // Arrange
+      dataArray[0] = [0, 0, 1];
+      dataArray[1] = [1, 0, 0];
+      dataArray[2] = [0, 0, 1];
+      // Act
+      const result = AppService.findNeighbours(dataArray, 1, 1);
+
+      // Assert
+      expect(result).toEqual(1);
+    });
   });
 });
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,28 +4,35 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AppService {
+  /**
+   * Return 1 if next generation of the cell should live, otherwise return 0
+   * @param arr Collection of actual cell
+   * @param x Cell coordinate of the abscissa
+   * @param y Cell coordinate of the ordinate
+   */
   static findNeighbours(
     arr: Array<Array<number>>,
-    i: number,
-    j: number
+    x: number,
+    y: number
   ): 0 | 1 {
     let neighboursAliveCounter: number = 0;
 
-    if (arr[i - 1] != undefined) {
-      neighboursAliveCounter += arr[i - 1][j - 1] ? 1 : 0;
-      neighboursAliveCounter += arr[i - 1][j] ? 1 : 0;
-      neighboursAliveCounter += arr[i - 1][j + 1] ? 1 : 0;
+    if (arr[x - 1] != undefined) {
+      neighboursAliveCounter += arr[x - 1][y - 1] ? 1 : 0;
+      neighboursAliveCounter += arr[x - 1][y] ? 1 : 0;
+      neighboursAliveCounter += arr[x - 1][y + 1] ? 1 : 0;
     }
-    neighboursAliveCounter += arr[i][j - 1] ? 1 : 0;
-    neighboursAliveCounter += arr[i][j + 1] ? 1 : 0;
-    if (arr[i + 1] != undefined) {
-      neighboursAliveCounter += arr[i + 1][j - 1] ? 1 : 0;
-      neighboursAliveCounter += arr[i + 1][j] ? 1 : 0;
-      neighboursAliveCounter += arr[i + 1][j + 1] ? 1 : 0;
+    neighboursAliveCounter += arr[x][y - 1] ? 1 : 0;
+    neighboursAliveCounter += arr[x][y + 1] ? 1 : 0;
+
+    if (arr[x + 1] != undefined) {
+      neighboursAliveCounter += arr[x + 1][y - 1] ? 1 : 0;
+      neighboursAliveCounter += arr[x + 1][y] ? 1 : 0;
+      neighboursAliveCounter += arr[x + 1][y + 1] ? 1 : 0;
     }
 
     // if actual cell is alive
-    if (arr[i][j] === 1) {
+    if (arr[x][y] === 1) {
       return neighboursAliveCounter < 2 || neighboursAliveCounter > 3 ? 0 : 1;
     } else {
       return neighboursAliveCounter === 3 ? 1 : 0;
